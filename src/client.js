65f0a843403ff60bcca57ec3e764196dd00efdcd
Erick Malta <erickmalta100@gmail.com>
2021-07-03 00:49:17 -0300

    feat(client): add tmi client

---
+++ src/client.js
@@ -0,0 +1,17 @@
+const tmi = require('tmi.js');
+
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN,
+  },
+  channels: [
+    process.env.CHANNEL_NAME,
+  ],
+};
+
+const client = new tmi.Client(opts);
+
+module.exports = {
+  client,
+}
